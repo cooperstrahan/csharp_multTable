44e8c1ccf25abf0e85ad4c1084028f4f616be8fa
Cooper Strahan <cooperstrahan@Coopers-MacBook-Pro.local>
2019-04-01 12:26:49 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace multiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multTable = new int[10,10];
+            
+            int rowLength = multTable.GetLength(0);
+            int colLength = multTable.GetLength(1);
+
+            for(int i = 0; i < rowLength; i++){
+                for(int j = 0; j < colLength; j++){
+                    multTable[i,j] = (i+1)*(j+1);
+                    Console.Write(multTable[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
